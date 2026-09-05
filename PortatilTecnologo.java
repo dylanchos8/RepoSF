c718b1b7048f01f7c7e95b6ae2e3a6f63dec1884
Yitan28 <brayanstivenesquivel12@gmail.com>
2026-09-04 21:02:46 -0500

    Segunda Hija

---
+++ PortatilTecnologo.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class PortatilTecnologo extends Dispositivo{
+    private String numeroFicha;
+    private int trimestre;
+
+    public PortatilTecnologo(String nombrePropietario, String marca, String numeroSerie, String numeroFicha, int trimestre) {
+        super(nombrePropietario, marca, numeroSerie);
+        this.numeroFicha = numeroFicha;
+        this.trimestre = trimestre;
+    }
+
+    @Override
+    public String obtenerAccesorios() {
+        return "Cargador del portátil (Programa Tecnólogo - Ficha: " + numeroFicha + ", Trimestre: " + trimestre + ")";
+    }
+
+}
