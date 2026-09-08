fe391548fb1170c92b9f2830b44805c90aa0074f
DieGoox837 <dbarretohernandez13@gmail.com>
2026-09-07 20:19:02 -0500

    Create Main.java
    Revision errores main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+     
+        PortatilTecnico portatilTecnico = new PortatilTecnico("Juan Pérez", "HP", "SN-001", "2589634");
+        PortatilTecnologo portatilTecnologo = new PortatilTecnologo("María Gómez", "Dell", "SN-002", "2589700", 3);
+        Dispositivo[] dispositivos = { portatilTecnico, portatilTecnologo };
+
+        for (Dispositivo d : dispositivos) {
+            d.registrarEntrada();
+            d.mostrarInformacion(); 
+        }
+
+        System.out.println("----------------------------------------");
+        dispositivos[0].registrarSalida();
+    }
+}
